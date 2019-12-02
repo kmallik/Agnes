63bfbbe4e7e3cd6900aa5e8b7b9b37a10cfb1910
Kaushik Mallik <kmallik@mpi-sws.org>
2019-12-02 11:35:41 +0100

    added safety game solver over the monitor

--- examples/test/test.cpp
+++ examples/test/test.cpp
@@ -12,6 +12,7 @@
 #include <array>
 #include <iostream>
 #include <cmath>
+#include <unordered_set>
 
 #include "Component.hpp"
 #include "SafetyAutomaton.hpp"
@@ -29,6 +30,13 @@ int main() {
     negotiation::SafetyAutomaton assume("Inputs/assume.txt");
     negotiation::SafetyAutomaton guarantee("Inputs/guarantee.txt");
     negotiation::Monitor monitor(c1,assume,guarantee);
+    
+    /* the safety game */
+    std::unordered_set<negotiation::abs_type> safe_states = {0,1,2,3,4};
+    std::vector<std::unordered_set<negotiation::abs_type>*> win_dom=monitor.solve_safety_game(safe_states);
+//    for (int i=0; i<win_dom.size(); i++) {
+//        delete win_dom[i];
+//    }
     
     return 1;
 }
--- src/Monitor.hpp
+++ src/Monitor.hpp
@@ -8,6 +8,8 @@
 #define MONITOR_HH_
 
 #include <vector>
+#include <queue>
+#include <unordered_set>
 
 #include "Component.hpp" /* for the definition of data types abs_type and abs_ptr_type */
 
@@ -25,6 +27,10 @@ class Monitor {
 public:
     /** @brief number of component states N **/
     abs_type no_states;
+    /** @brief number of assume automaton states **/
+    abs_type no_assume_states;
+    /** @brief number of guarantee automaton states **/
+    abs_type no_guarantee_states;
     /** @brief number of control inputs M **/
     abs_type no_control_inputs;
     /** @brief number of internal disturbance inputs P **/
@@ -37,12 +43,16 @@ public:
 //    std::vector<abs_ptr_type> pre_ptr;
 //    /** @brief vector[N*M*P] saving the number of pre for each state-control input-disturbance input tuple (i,j,k) **/
 //    std::vector<abs_type> no_pre;
-//    /** @brief vector[N*M*P] saving the number of post for each state-control input-disturbance input tuple (i,j,k) **/
-//    std::vector<abs_type> no_post;
+    /** @brief vector[N] saving the number of post for each state i **/
+    std::vector<abs_type> no_post;
+    /** @brief vector[N] saving the set of allowed inputs for each state i **/
+    std::vector<std::unordered_set<abs_type>*> valid_input;
 public:
     /* copy constructor */
     Monitor(const Monitor& other) {
         no_states=other.no_states;
+        no_assume_states=other.no_assume_states;
+        no_guarantee_states=other.no_guarantee_states;
         no_control_inputs=other.no_control_inputs;
         no_dist_inputs=other.no_dist_inputs;
         abs_type size = no_states*no_control_inputs*no_dist_inputs;
@@ -53,13 +63,20 @@ public:
 //        pre=other.pre;
 //        pre_ptr=other.pre_ptr;
 //        no_pre=other.no_pre;
-//        no_post=other.no_post;
+        no_post=other.no_post;
+        for (int i=0; i<size; i++) {
+            valid_input[i]=other.valid_input[i];
+        }
     }
     /* Destructor */
     ~Monitor() {
         for (int i=0; i<no_states*no_control_inputs*no_dist_inputs; i++) {
             delete pre[i];
         }
+        for (int i=0; i<no_states; i++) {
+            delete valid_input[i];
+        }
+        delete[] pre;
     }
     /* constructor */
     Monitor(Component& comp, SafetyAutomaton& assume, SafetyAutomaton& guarantee) {
@@ -81,20 +98,29 @@ public:
         /* first make sure that the assumption and guarantees are deterministic safety automata */
         /* now compute the product */
         no_states=comp.no_states*(assume.no_states-1)*(guarantee.no_states-1)+2;
+        no_assume_states=assume.no_states;
+        no_guarantee_states=guarantee.no_states;
         no_control_inputs=comp.no_control_inputs;
         no_dist_inputs=comp.no_dist_inputs;
+        no_post.assign(no_states,0);
+        no_post[0]=no_control_inputs*no_dist_inputs;
+        no_post[1]=no_control_inputs*no_dist_inputs;
         pre=new std::vector<abs_type>*[no_states*no_control_inputs*no_dist_inputs];
         for (int i=0; i<no_states*no_control_inputs*no_dist_inputs; i++) {
             std::vector<abs_type> *v=new std::vector<abs_type>;
             pre[i]=v;
         }
+        for (int i=0; i<no_states; i++) {
+            std::unordered_set<abs_type> *a = new std::unordered_set<abs_type>;
+            valid_input.push_back(a);
+        }
         for (int ic=0; ic<comp.no_states; ic++) {
-            for (int ia=1; ia<assume.no_states; ia++) {
-                for (int ig=1; ig<guarantee.no_states; ig++) {
+            for (int ia=1; ia<no_assume_states; ia++) {
+                for (int ig=1; ig<no_guarantee_states; ig++) {
                     for (int j=0; j<no_control_inputs; j++) {
                         for (int k=0; k<no_dist_inputs; k++) {
                             /* the pre state index for the tuple (ic,ia,ig)*/
-                            abs_type im = state_ind(ic,ia,ig,assume.no_states,guarantee.no_states);
+                            abs_type im = state_ind(ic,ia,ig,no_assume_states,no_guarantee_states);
                             /* the post component states */
                             std::vector<abs_type> ic2 = *comp.post[comp.addr(ic,j,k)];
                             /* the post assumption state (singleton) */
@@ -113,7 +139,9 @@ public:
                                     ig2 = (*guarantee.post[guarantee.addr(ig,comp.state_to_output[*it])])[0];
                                 }
                                 /* the post state tuple index */
-                                abs_type im2 = state_ind(*it,ia2,ig2,assume.no_states,guarantee.no_states);
+                                abs_type im2 = state_ind(*it,ia2,ig2,no_assume_states,no_guarantee_states);
+                                no_post[im]++;
+                                valid_input[im]->insert(j);
                                 if (ia2!=0 && ig2!=0) {
                                     pre[addr_pre(im2,j,k)]->push_back(im);
                                 } else if (ia2==0 && ig2!=0) {
@@ -134,6 +162,67 @@ public:
                 pre[addr_pre(1,j,k)]->push_back(1);
             }
         }
+    }
+    /*! Solve safety game.
+     *
+     * The state reject_A (ind=0) is safe but reject_G (ind=1) is unsafe by default.
+     *
+     * \param[in] component_safe_states         indices of safe states
+     * \param[out] D                             winning state-input pairs */
+    std::vector<std::unordered_set<abs_type>*> solve_safety_game(const std::unordered_set<abs_type> component_safe_states) {
+        /* safe states */
+        std::unordered_set<abs_type> monitor_safe_states;
+        for (std::unordered_set<abs_type>::iterator it=component_safe_states.begin(); it!=component_safe_states.end(); ++it) {
+            for (abs_type j=1; j<no_assume_states; j++) {
+                for (abs_type k=1; k<no_guarantee_states; k++) {
+                    monitor_safe_states.insert(state_ind(*it,j,k,no_assume_states,no_guarantee_states));
+                }
+            }
+        }
+        std::queue<abs_type> Q; /* FIFO queue of bad states */
+        std::unordered_set<abs_type> E; /* bad states */
+        std::vector<std::unordered_set<abs_type>*> D; /* set of valid inputs indexed by the state indices */
+        for (int i=0; i<no_states; i++) {
+            std::unordered_set<abs_type>* s = new std::unordered_set<abs_type>;
+            D.push_back(s);
+        }
+        /* state index 0 is safe but 1 is unsafe */
+        Q.push(1);
+        E.insert(1);
+        for (abs_type i=0; i<no_control_inputs; i++) {
+            D[0]->insert(i);
+        }
+        /* initialize Q, E, D for all the states other than 0,1 */
+        for (int i=2; i<no_states; i++) {
+            if (!isMember<abs_type>(monitor_safe_states,i) || no_post[i]==0) {
+                Q.push(i);
+                E.insert(i);
+            } else {
+                for (std::unordered_set<abs_type>::iterator it=valid_input[i]->begin(); it!=valid_input[i]->end(); ++it) {
+                    D[i]->insert(*it);
+                }
+            }
+        }
+        /* iterate until Q is empty, i.e. when a fixed point is reached*/
+        while (Q.size()!=0) {
+            abs_type x = Q.front();
+            Q.pop();
+            for (int j=0; j<no_control_inputs; j++) {
+                for (int k=0; k<no_dist_inputs; k++) {
+                    abs_type x2 = addr_pre(x,j,k);
+                    for (std::vector<abs_type>::iterator it=pre[x2]->begin(); it!=pre[x2]->end(); ++it) {
+                        /* remove all the control inputs from the pre-states of x which lead to x */
+                        D[*it]->erase(j);
+                        if (D[*it]->size()==0 && !isMember<abs_type>(E,*it)) {
+                            Q.push(*it);
+                            E.insert(*it);
+                        }
+                    }
+                }
+            }
+        }
+        /* winning strategy */
+        return D;
     }
     /*! Address of pre in pre array.
      * \param[in] i           state index
@@ -167,6 +256,19 @@ public:
             }
         }
         return (ic*(na-1)*(ng-1) + (ia-1)*(ng-1) + (ig-1) + 2); /* the -1 with ia and ig are to shift all the ia and ig indeces leftwards, since the reject state is not used in the product. the +2 in the end is to make sure that reject states of the monitor 0,1 are indeed reserved. */
+    }
+private:
+    /*! Membership querry for an unordered set.
+     *  \param[in] S     the unordered set
+     *  \param[in] e    the element */
+    template<class T>
+    bool isMember(const std::unordered_set<T>& S, const T& e) {
+        typename std::unordered_set<T>::const_iterator it=S.find(e);
+        if (it==S.end()) {
+            return false;
+        } else {
+            return true;
+        }
     }
 };/* end of class defintions*/
 }/* end of namespace negotiation */
